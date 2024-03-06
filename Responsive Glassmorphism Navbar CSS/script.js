406ad03257b265f850bc3308ecf73ce52ef3f4ed
Nouredine18 <159436420+Nouredine18@users.noreply.github.com>
2024-03-06 08:57:49 +0100

    change

---
+++ Responsive Glassmorphism Navbar CSS/script.js
@@ -0,0 +1,23 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navbar = document.querySelector('.navbar');
+const navbg = document.querySelector('.nav-bg');
+
+menuIcon.addEventListener('click', () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+    navbg.classList.toggle('active');
+});
+
+
+function openLoginPage() {
+    document.querySelector(".reg").classList.remove("show-page");
+    document.querySelector(".login").classList.add("show-page");
+    document.getElementById("login-action").classList.add("show");
+    document.getElementById("reg-action").classList.remove("show");
+  }
+  function openRegPage() {
+    document.querySelector(".reg").classList.add("show-page");
+    document.querySelector(".login").classList.remove("show-page");
+    document.getElementById("reg-action").classList.add("show");
+    document.getElementById("login-action").classList.remove("show");
+  }
