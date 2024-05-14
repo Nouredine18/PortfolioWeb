06d5cc268882675ded5df39d52c780bd51f92d4f
Nouredine18 <159436420+Nouredine18@users.noreply.github.com>
2024-05-14 14:53:01 +0200

    DANTE

--- docs/script.js
+++ docs/script.js
@@ -80,29 +80,70 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     updateLogoutButton();
 });
 
-/* function checkLoggedIn() {
-    if (isLoggedIn()) {
-      // User is logged in, show the logged in content
-      document.querySelector('.navbar').style.display = 'block';
-    } else {
-      // User is not logged in, hide the content
-      document.querySelector('.navbar').style.display = 'none';
-    }
-  }
-
-checkLoggedIn();  */
- 
-/* function checkLoggedIn() {
-    if (!isLoggedIn()) {
-        window.location.href = "index.html";
-    }
+function mySubmit(obj) {
+  var pwdObj = document.getElementById('pwd');
+  var hashObj = new jsSHA("SHA-512", "TEXT", {numRounds: 1});
+  hashObj.update(pwdObj.value);
+  var hash = hashObj.getHash("HEX");
+  pwdObj.value = hash;
+  console.log("")
 }
 
-checkLoggedIn();
+ 
+var myInput = document.getElementById("password");
+var letter = document.getElementById("letter");
+var capital = document.getElementById("capital");
+var number = document.getElementById("number");
+var length = document.getElementById("length");
 
+// When the user clicks on the password field, show the message box
+myInput.onfocus = function() {
+  document.getElementById("message").style.display = "block";
+}
 
- */
+// When the user clicks outside of the password field, hide the message box
+myInput.onblur = function() {
+  document.getElementById("message").style.display = "none";
+}
 
-/*if (!isLoggedIn() && window.location.pathname !== "/index.html") {
-    window.location.href = "index.html";
-}  */
+// When the user starts to type something inside the password field
+myInput.onkeyup = function() {
+  // Validate lowercase letters
+  var lowerCaseLetters = /[a-z]/g;
+  if(myInput.value.match(lowerCaseLetters)) {  
+    letter.classList.remove("invalid");
+    letter.classList.add("valid");
+  } else {
+    letter.classList.remove("valid");
+    letter.classList.add("invalid");
+  }
+  
+  // Validate capital letters
+  var upperCaseLetters = /[A-Z]/g;
+  if(myInput.value.match(upperCaseLetters)) {  
+    capital.classList.remove("invalid");
+    capital.classList.add("valid");
+  } else {
+    capital.classList.remove("valid");
+    capital.classList.add("invalid");
+  }
+
+  // Validate numbers
+  var numbers = /[0-9]/g;
+  if(myInput.value.match(numbers)) {  
+    number.classList.remove("invalid");
+    number.classList.add("valid");
+  } else {
+    number.classList.remove("valid");
+    number.classList.add("invalid");
+  }
+  
+  // Validate length
+  if(myInput.value.length >= 8) {
+    length.classList.remove("invalid");
+    length.classList.add("valid");
+  } else {
+    length.classList.remove("valid");
+    length.classList.add("invalid");
+  }
+}  
